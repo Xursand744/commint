ce5bd794d8cbd4c7dd1cb366c27596d1e2a68eba
xursand <xursand@D>
2023-06-27 15:48:24 +0300

    commenta

--- style.js
+++ style.js
@@ -1,20 +1,35 @@
 const TOKEN = '6083774594:AAGjkti-sdnmNkGpQvwplA3WOR5-HtW16OQ'
 const CHAT_ID = '-1001947436112'
 const URL = `https://api.telegram.org/bot${ TOKEN }/sendMessage`
-
+const success = document.getElementById('success')
 
 document.getElementById('tg').addEventListener('submit', function(e) {
     e.preventDefault()
 
-    let massage = 'Saytdan habar keldi!!!\n'
-    massage += `Yuborovchi: ${this.name.value}\n`
-    massage += `Matn: ${this.text.value}\n`
+    let massage = 'Saytdan habar keldi!!!\n\n'
+    massage += `YUBORUVCHI: ${ this.name.value}\n\n`
+    massage += `MATN: ${ this.text.value}\n`
 
 
     axios.post(URL,{
         chat_id:CHAT_ID,
         porse_mod:'html',
         text:massage
+    })
+
+    .then((res) => {
+        this.name.value = ''
+        this.text.value = ''
+        success.innerHTML = 'Xabar yuborildi!'
+        success.style.display = 'block'
+    })
+
+    .catch((err) => {
+        console.warn(err);
+    })
+
+    .finally(() => {
+        console.log('Tugatildi');
     })
 
 
